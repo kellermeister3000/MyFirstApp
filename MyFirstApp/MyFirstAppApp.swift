3f23a392f45caabc824ffda4e5188029fe130146
Philip Keller <philipjoelkeller@gmail.com>
2023-03-01 10:54:18 -0800

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Philip Keller on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
